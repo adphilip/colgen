32660eb10e7ec6aa4730e5ed46121433589569d8
Adrian Filipescu <adrian.filipescu@gmail.com>
2016-12-30 08:48:32 +0200

    Using Map example

--- src/main/java/com/softcoaching/ro/colgen.java
+++ src/main/java/com/softcoaching/ro/colgen.java
@@ -2,20 +2,41 @@ package com.softcoaching.ro;
 /**
  * Created by adrian on 12/17/16.
  */
+
 import java.util.*;
-class colgen{
-    public static void main(String args[]){
-        ArrayList<String> list=new ArrayList<String>();
+
+class colgen {
+    public static void main(String args[]) {
+        //1.Using ArrayList
+        ArrayList<String> list = new ArrayList<String>();
         list.add("rahul");
         list.add("jai2016");
         //list.add(32);//compile time error
 
-        String s=list.get(1);//type casting is not required
-        System.out.println("element is: "+s);
+        String s = list.get(1);//type casting is not required
+        System.out.println("element is: \n" + s);
 
-        Iterator<String> itr=list.iterator();
-        while(itr.hasNext()){
+        Iterator<String> itr = list.iterator();
+        while (itr.hasNext()) {
             System.out.println(itr.next());
+        }
+
+
+        //2. Using Map
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1, "vijay");
+        map.put(4, "umesh");
+        map.put(2, "ankit");
+
+        //Now use Map.Entry for Set and Iterator
+        Set<Map.Entry<Integer, String>> set = map.entrySet();
+
+        System.out.println("\nMap  is: ");
+
+        Iterator<Map.Entry<Integer, String>> itrMap = set.iterator();
+        while (itrMap.hasNext()) {
+            Map.Entry e = itrMap.next();//no need to typecast
+            System.out.println(e.getKey() + " " + e.getValue());
         }
     }
 }
